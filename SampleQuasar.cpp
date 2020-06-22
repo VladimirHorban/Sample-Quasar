e38d4245763bad8d956e5daecfd8f48c6b4ea63f
VladimirHorban <54627744+VladimirHorban@users.noreply.github.com>
2020-06-22 12:30:36 +0300

    Update

---
+++ SampleQuasar.cpp
@@ -0,0 +1,9 @@
+﻿#include "SampleQuasar.h"
+
+int WINAPI main(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR cmdLine, INT cmdCount)
+{
+	CoreMinimal core("SampleQuasar", 800, 600, currentInstance, "E:/VSProjects/SampleQuasar/Assets/Shaders/vert.spv", "E:/VSProjects/SampleQuasar/Assets/Shaders/frag.spv");
+	core.run();
+	
+	return 0;
+}
---
+++ SampleQuasar.h
@@ -0,0 +1,9 @@
+﻿// SampleQuasar.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "Quasar/API/Headers/CoreMinimal.h"
+
+// TODO: Reference additional headers your program requires here.
